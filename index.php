dc08748a218c97a1c62c5eeb875a2350cf3898dd
LIGHT <gaireprakash99@gmail.com>
2025-04-25 10:50:25 +0545

    second one

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo '<h1>तानसेन नगरपालिका !</h1>';
-echo'<img src="Evening_View_of_Tansen.jpg"width="700">';
+echo '<h1>तानसेन  !</h1>';
+echo'<img src="Evening_View_of_Tansen.jpg"width="600">';
 ?>
